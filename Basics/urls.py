c86173b88d62b86f21fafa883aa3b8f7ca68d867
Nivya1 <nivya.a@consultadd.com>
2016-12-23 08:07:08 +0530

    Login working application

--- Basics/urls.py
+++ Basics/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from login.views import *
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home)
 ]
--- login/forms.py
+++ login/forms.py
@@ -1,11 +1,11 @@
 import re
 from django import forms
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as __
+from django.utils.translation import ugettext_lazy as _
 
 class RegistrationForm(forms.Form):
-	username = forms.RegexField(regex = r'^\w+$', widget = forms.TextInput(attrs = dict(required = True, max_length = 30)), label = ("Username"), error_messages = {'invalid':_("This value must contain only letters, numbers and underscores.")})
-	email =forms.EmailField(widget = forms.TextInput(attrs = dict(required = True, max_length = 30)), label = _("Email address"))
+	username = forms.RegexField(regex = r'^\w+$', widget = forms.TextInput(attrs = dict(required = True, max_length = 30)), label = _("Username"), error_messages = {'invalid': _("This value must contain only letters, numbers and Underscores.")})
+	email = forms.EmailField(widget = forms.TextInput(attrs = dict(required = True, max_length = 30)), label = _("Email Address"))
 	password1 = forms.CharField(widget = forms.PasswordInput(attrs = dict(required = True, max_length = 30, render_value = False)), label = _("Password"))
 	password2 = forms.CharField(widget = forms.PasswordInput(attrs = dict(required = True, max_length = 30, render_value = False)), label = _("Password(again)"))
 
@@ -14,12 +14,10 @@ class RegistrationForm(forms.Form):
 			user = User.objects.get(username__iexact = self.cleaned_data['username'])
 		except User.DoesNotExist:
 			return self.cleaned_data['username']
-		raise form.ValidationError(_("The username already exists. Please try another one."))
+		raise forms.ValidationError(_("The username already exists. Please try another one."))
 
 	def clean(self):
 		if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
 			if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-				raise form.ValidationError(_("The two password fields didnot match."))
+				raise forms.ValidationError(_("The two password fields did not match."))
 		return self.cleaned_data
-
-
